30725c22767a8a86a599c7101490e5e8a2d9a6a3
amrfekryy <amrfekryy@gmail.com>
2025-03-02 18:10:26 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body
         className={`${dmSans.variable} antialiased min-h-screen w-screen bg-[#FAFFFF] md:bg-[#F5F5F5]`}
       >
+        <Analytics />
         <SidebarProvider>
           <div className="flex flex-col h-full w-full">
             <AppNavbar />
