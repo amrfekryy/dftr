d7680de7655d249ac5caacece0dc8513246b48cb
amrfekryy <amrfekryy@gmail.com>
2025-03-02 18:27:18 +0200

    fix svg props

--- components/icons/messaging.tsx
+++ components/icons/messaging.tsx
@@ -20,9 +20,9 @@ export const MessagingIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
       <path
         d="M24.2107 16.75C24.2107 17.413 23.9473 18.0489 23.4785 18.5178C23.0096 18.9866 22.3737 19.25 21.7107 19.25H6.71069L1.71069 24.25V4.25C1.71069 3.58696 1.97409 2.95107 2.44293 2.48223C2.91177 2.01339 3.54765 1.75 4.21069 1.75H21.7107C22.3737 1.75 23.0096 2.01339 23.4785 2.48223C23.9473 2.95107 24.2107 3.58696 24.2107 4.25V16.75Z"
         stroke="#E6E6E6"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -48,9 +48,9 @@ export const MessagingMobileIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
       <path
         d="M13.4173 9.26533C13.4173 9.62669 13.2737 9.97325 13.0182 10.2288C12.7627 10.4843 12.4161 10.6278 12.0548 10.6278H3.87978L1.15479 13.3528V2.45283C1.15479 2.09147 1.29833 1.74492 1.55385 1.4894C1.80937 1.23388 2.15593 1.09033 2.51728 1.09033H12.0548C12.4161 1.09033 12.7627 1.23388 13.0182 1.4894C13.2737 1.74492 13.4173 2.09147 13.4173 2.45283V9.26533Z"
         stroke="#7D7D7D"
-        stroke-width="1.09"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.09"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- components/icons/notifications.tsx
+++ components/icons/notifications.tsx
@@ -20,16 +20,16 @@ export const NotificationsIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
       <path
         d="M26.9607 11.6667C26.9607 9.3461 26.0388 7.12051 24.3979 5.47956C22.7569 3.83862 20.5313 2.91675 18.2107 2.91675C15.89 2.91675 13.6645 3.83862 12.0235 5.47956C10.3826 7.12051 9.46069 9.3461 9.46069 11.6667C9.46069 21.8751 5.08569 24.7917 5.08569 24.7917H31.3357C31.3357 24.7917 26.9607 21.8751 26.9607 11.6667Z"
         stroke="#E6E6E6"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.7336 30.625C20.4772 31.067 20.1092 31.4339 19.6664 31.6889C19.2236 31.9439 18.7216 32.0782 18.2107 32.0782C17.6997 32.0782 17.1977 31.9439 16.7549 31.6889C16.3121 31.4339 15.9441 31.067 15.6877 30.625"
         stroke="#E6E6E6"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
@@ -37,13 +37,7 @@ export const NotificationsIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
 
 export const NotificationsMobileIcon: React.FC<
   React.SVGProps<SVGSVGElement>
-> = ({
-  width = 18,
-  height = 18,
-  fill = "none",
-  className = "",
-  ...props
-}) => {
+> = ({ width = 18, height = 18, fill = "none", className = "", ...props }) => {
   return (
     <svg
       width={width}
@@ -57,16 +51,16 @@ export const NotificationsMobileIcon: React.FC<
       <path
         d="M14.0066 6.47841C14.0066 5.25362 13.52 4.079 12.654 3.21295C11.7879 2.3469 10.6133 1.86035 9.38851 1.86035C8.16372 1.86035 6.9891 2.3469 6.12305 3.21295C5.257 4.079 4.77045 5.25362 4.77045 6.47841C4.77045 11.8661 2.46143 13.4055 2.46143 13.4055H16.3156C16.3156 13.4055 14.0066 11.8661 14.0066 6.47841Z"
         stroke="#7D7D7D"
-        stroke-width="1.31944"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.31944"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.72 16.4841C10.5847 16.7174 10.3905 16.911 10.1568 17.0456C9.92312 17.1802 9.65818 17.2511 9.3885 17.2511C9.11882 17.2511 8.85388 17.1802 8.62019 17.0456C8.3865 16.911 8.19228 16.7174 8.05696 16.4841"
         stroke="#7D7D7D"
-        stroke-width="1.31944"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.31944"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
